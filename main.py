fcd21bd51cb6c655ee09fd5581f5169d9b40116e
yoran <129041844+sharpySpan@users.noreply.github.com>
2025-09-10 14:13:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 import cv2
 import time
 import winsound
+from pyenttec import DMXConnection
 import pygame
 
-pygame.mixer.init()
-sound = pygame.mixer.Sound("241227_002040_Tr1.WAV")
-
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-cap = cv2.VideoCapture(0)
-
+# variables
 min_frames_tracked = 3
 last_beep_time = 0
 frames_with_face = 0
+COM_PORT = 'COM3'
+DMX_START_ADDRESS = 1
+
+# setup dmx
+def set_light(dmx, intensity=0, red=0, green=0, blue=0):
+    dmx.set_channel(DMX_START_ADDRESS, intensity)
+    dmx.set_channel(DMX_START_ADDRESS + 1, red)
+    dmx.set_channel(DMX_START_ADDRESS + 2, green)
+    dmx.set_channel(DMX_START_ADDRESS + 3, blue)
+    dmx.render()
+
+# Dmx control
+dmx = DMXConnection(COM_PORT)
+set_light(dmx, intensity=100, red=100, green=0, blue=0)
+
+# ambient sound
+pygame.mixer.init()
+sound = pygame.mixer.Sound("ambient video.mp3")
+sound.play()
+
+# face detection
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
 
 while True:
-    sound.play()
     ret, frame = cap.read()
-    if not ret:
-        break
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -32,7 +47,9 @@ while True:
 
         if frames_with_face >= min_frames_tracked:
             if time.time() - last_beep_time > delay:
-                winsound.Beep(1000, 200)
+                set_light(dmx, intensity=255, red=255, green=0, blue=0)
+                winsound.Beep(1000, 250)
+                set_light(dmx, intensity=100, red=100, green=0, blue=0)
                 last_beep_time = time.time()
 
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
